7530b916fe330b9b94653e48f21d3a082687f172
hyunsu <mypace0600@gmail.com>
2024-04-05 16:43:01 +0900

    getUsername 에서 getEmail로 변경

--- src/main/java/com/project/myBlog/config/PrincipalDetail.java
+++ src/main/java/com/project/myBlog/config/PrincipalDetail.java
@@ -30,7 +30,7 @@ public class PrincipalDetail  implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getUsername();
+        return user.getEmail();
     }
 
     @Override
