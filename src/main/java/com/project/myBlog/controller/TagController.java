a67be9352b73235a4728e968b68efd35e4b49c34
hyunsu <mypace0600@gmail.com>
2024-04-23 16:31:56 +0900

    :wrench: 불필요 코드 삭제

--- src/main/java/com/project/myBlog/controller/TagController.java
+++ src/main/java/com/project/myBlog/controller/TagController.java
@@ -4,7 +4,6 @@ import com.project.myBlog.service.PostService;
 import com.project.myBlog.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
