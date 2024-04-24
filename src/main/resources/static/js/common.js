44ab30166ddc470332169f5b9c435cadd94806ef
ruukr8080 <ruukr8080@gmail.com>
2024-04-24 17:10:34 +0900

    💄 url에 따른 버튼 스타일 추가 수정2

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,13 +1,32 @@
-let container = document.getElementById('line_container_div');
-let Lines = 9;
-let l = 10;
-let n = 2;
-for (let i = 1; i < Lines; i++) {
-    const line = document.createElement('div');
-    line.style.height = `calc(86vh - ${i*10}vh`; // height
-    line.style.marginTop = `calc(10px + ${4.9*10*i}px)`; // margin-top
-    line.style.marginLeft = `calc(0px + ${1}vw)`; // fallfrom left
-    line.style.width = '1px';
-    container.appendChild(line);
+let common = {
+    init : function () {
+        document.addEventListener("DOMContentLoaded", this.drawLines);
+        document.addEventListener("DOMContentLoaded", this.urlCheck);
+    },
+
+    drawLines : function (){
+        let container = document.getElementById('line_container_div');
+        let Lines = 9;
+        let l = 10;
+        let n = 2;
+        for (let i = 1; i < Lines; i++) {
+            const line = document.createElement('div');
+            line.style.height = `calc(86vh - ${i*10}vh`; // height
+            line.style.marginTop = `calc(10px + ${4.9*10*i}px)`; // margin-top
+            line.style.marginLeft = `calc(0px + ${1}vw)`; // fallfrom left
+            line.style.width = '1px';
+            container.appendChild(line);
+        }
+    },
+
+    urlCheck: function (){
+        let url = window.location.pathname;
+        let id = "nav_"+url.replace("/","").split("/")[0];
+        let currentNavBtn = document.getElementById(id);
+        currentNavBtn.classList.add("special_nav_btn");
+    }
 }
+
+
+common.init();
 
