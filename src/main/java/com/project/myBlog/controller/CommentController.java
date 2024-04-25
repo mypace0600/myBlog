7154328b765a75b926a39b94cbddca6ada14c9b8
mypace0600 <mypace0600@gmail.com>
2024-04-25 16:32:07 +0900

    :wrench: 깃허브 로그인 이후 댓글작성 가능하도록 수정

--- src/main/java/com/project/myBlog/controller/CommentController.java
+++ src/main/java/com/project/myBlog/controller/CommentController.java
@@ -4,12 +4,19 @@ import com.project.myBlog.common.ResponseDto;
 import com.project.myBlog.config.PrincipalDetail;
 import com.project.myBlog.dto.CommentDto;
 import com.project.myBlog.entity.Post;
+import com.project.myBlog.entity.User;
 import com.project.myBlog.service.CommentService;
 import com.project.myBlog.service.PostService;
+import com.project.myBlog.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,12 +28,36 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class CommentController {
 
     private final CommentService commentService;
+    private final UserService userService;
 
     @PostMapping("/comment")
     @ResponseBody
     public ResponseDto<Integer> commentAdd(@RequestBody CommentDto commentDto, @AuthenticationPrincipal PrincipalDetail principal){
-        log.debug("@@@@@@@@@@@@@@@@@@ commentDto :{}",commentDto.toString());
-        commentService.save(commentDto, principal.getUser());
+        User user = null;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (null == principal) {
+            DefaultOAuth2User oauthPrincipal = (DefaultOAuth2User) auth.getPrincipal();
+            if(isOAuth(auth)){
+                user = userService.findOauthUser(oauthPrincipal).orElseThrow(EntityNotFoundException::new);
+            }
+        } else {
+            user = principal.getUser();
+        }
+        commentService.save(commentDto, user);
+
         return new ResponseDto<>(HttpStatus.OK.value(), 1);
+    }
+
+    // oauth 로그인인지 확인
+    private boolean isOAuth(Authentication auth) {
+        if (auth == null) {
+            return false;
+        }
+        for (GrantedAuthority authority : auth.getAuthorities()) {
+            if (authority.getAuthority().equals("OAUTH")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
